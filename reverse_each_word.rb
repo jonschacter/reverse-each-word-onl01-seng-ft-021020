34c0fe5c930cda93cf93edb684c80c7b8df7626c
Jon Schacter <schacter.jon@gmail.com>
2020-02-11 21:32:40 +0000

    Done.

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,9 +1,3 @@
 def reverse_each_word(sentence)
-  sentence_array = sentence.split(" ")
-
-  reverse_sentence = sentence_array.collect do |word|
-    word.reverse
-  end
-
-  reverse_sentence.join(" ")
+  sentence.split.collect {|word| word.reverse}.join(" ")
 end
